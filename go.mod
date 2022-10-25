a6eb5beec2f93c093b8661090b87aeabb9b7ba75
pakkunat <pakkun_kh@fol.hi-ho.ne.jp>
2022-10-25 22:44:45 +0900

    updated

---
+++ go.mod
@@ -0,0 +1,3 @@
+module https2
+
+go 1.19
---
+++ https2.go
@@ -0,0 +1,23 @@
+// The executable program need in main package.
+package main
+
+import (
+	// for Fprintf.
+	"fmt"
+	// for ResponseWriter, Request, HandleFunc, ListenAndServe.
+	"net/http"
+)
+
+// Handler function.
+func handler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+}
+
+// main function.
+func main() {
+	// Subscribe handler.
+	mux := 
+	//http.HandleFunc("/", handler)
+	// Listen for port 443.
+	http.ListenAndServeTLS("", "/usr/local/bin/go/https2/certificate.crt", "/usr/local/bin/go/https2/private.key", handler)
+}
